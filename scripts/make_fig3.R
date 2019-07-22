a9dcd86ba23e7df59625560608e7f648f2121bcb
Stuart Lee <lee.s@wehi.edu.au>
2019-07-22 11:52:21 +1000

    update for coverage plot refactor

--- scripts/make_fig3.R
+++ scripts/make_fig3.R
@@ -10,8 +10,9 @@ library(superintronic)
 library(ggplot2)
 
 cvg <- readRDS(here::here("data", "parts-coverage.rds")) %>% 
-  filter(CellLine == "HCC287") %>% 
-  mutate(log_score = log2(score + 0.5)) 
+  filter(CellLine == "HCC287") %>%
+  mutate(log_score = log2(score + 0.5),
+         CellLine = ifelse(CellLine == "HCC287", "HCC827", CellLine)) 
 
 # our selected genes
 parts <- readRDS(here::here("data", "filtered-annotation.rds")) 
@@ -104,73 +105,41 @@ fig3a <- ggplot(tbl, aes(x = bin, y = lumpy, colour = Kit, group = Sample)) +
   facet_grid(feature_type ~ cat, scales = "free_y") +
   scale_color_manual(values = c("#404040", "#bababa")) +
   theme_bw() +
-  theme(aspect.ratio = 1, 
-        legend.position = "bottom",
+  theme(aspect.ratio = 1,
+        legend.position = "top",
         legend.title = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        axis.title.x = element_blank()) +
-  labs(y = "Relative Log coverage")
+        axis.ticks.x = element_blank()
+        ) +
+  labs(y = "Relative Log-coverage", 
+       x = "5'---Genebody---3'")
 
 ggsave(here::here("img", "Fig3a.pdf"), fig3a, height = 12, width = 16)  
 
-#' pretty coverage
-coverage_plot <- function(target) {
-  cvg_rng <- filter_by_overlaps(cvg, target) %>% 
-    mutate(strand = strand(target),
-           var = paste0("Kit: ", Kit, " Cellline: ", CellLine)) %>% 
-    group_by(var)
-  
-  cvg_rng <- disjoin_ranges_directed(cvg_rng,
-                                     score = mean(log_score),
-                                     feature = unlist(unique(feature_type)))
-  
-  cvg_rng <- mutate(cvg_rng,
-                    alpha = dplyr::case_when(
-                      score > 3 & feature == "intron" ~ 1,
-                      score <= 3 & feature == "intron"~ 0.5,
-                      TRUE ~ 1
-                    ))
-  
-  p <- ggplot(as.data.frame(cvg_rng), 
-              aes(x = start, xend = end, y = 0, yend = score)) +
-    geom_segment(aes(colour = feature, alpha = alpha)) +
-    superintronic:::rescale_by_width(cvg_rng) +
-    facet_wrap(~var, ncol = 1) +
-    ylim(0, NA) +
-    guides(colour = FALSE, alpha = FALSE) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(subtitle = paste("Coverage over", target$gene_name),
-         y = "Log-coverage") +
-    theme_bw() +
-    theme(axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          strip.text = element_text(hjust = 0, size = 8),
-          strip.background = element_blank()) +
-    expand_limits(y = 0)
-  
-  segments <- superintronic:::flatten_parts(target)
-  track <- view_segments(segments, color = feature_type) +
-    theme(axis.text.x = element_text(size = 8))
-  
-  patchwork::wrap_plots(p, track, 
-                        ncol = 1, 
-                        heights = c(2, 0.25))
-  
-}
+
+source(here::here("R", "prettycov.R"))
+
 # replicate figure 3b, two short two long
 targets <- filter(parts, 
                   gene_id %in% c("ENSG00000136997.17", "ENSG00000117525.13", 
                                  "ENSG00000196937.10", "ENSG00000196428.12")
 )
 
-short <- lapply(1:2, function(.x) coverage_plot(targets[.x,])) %>% 
+short <- lapply(1:2, 
+                function(.x) pretty_cov_plot(cvg, parts, 
+                                             targets[.x,], alpha = TRUE, heights = c(2, 0.25))) %>% 
   patchwork::wrap_plots(ncol = 2, guides = "keep")
 
-long <- lapply(3:4, function(.x) coverage_plot(targets[.x,])) %>% 
+long <- lapply(3:4, function(.x) pretty_cov_plot(cvg, parts, 
+                                                 targets[.x,], alpha = TRUE, heights = c(2, 0.25))) %>% 
   patchwork::wrap_plots(ncol = 2, guides = "keep")
 
 fig3b <- patchwork::wrap_plots(short, long, nrow = 2, guides = "keep")
 
 ggsave(here::here("img", "Fig3b.pdf"), fig3b, height = 12, width = 16)  
 
+fig3 <- patchwork::wrap_plots(fig3a, fig3b, ncol = 2, guides = "keep",
+                              widths = c(3,4.75))
+
+ggsave(here::here("img", "Fig3.png"), fig3, width = 16, height = 8, 
+       dpi = "retina")
