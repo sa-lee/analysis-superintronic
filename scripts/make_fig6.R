65f22d040501c2b0f5f94c764b78334d43cc9836
Stuart Lee <lee.s@wehi.edu.au>
2019-07-22 11:40:47 +1000

    refactoring coverage plot

--- scripts/make_fig6.R
+++ scripts/make_fig6.R
@@ -7,71 +7,36 @@
 #' 
 #' 
 #' pretty coverage
-coverage_plot <- function(.y) {
-  target <- filter(parts, gene_id == !!.y$gene_id)
-  
-  # set up parts
-  cvg_rng <- as(lapply(cvg, function(x) join_parts(x, target)), "GRangesList")
-  md <- DataFrame(Sample = Rle(names(cvg_rng), lengths(cvg_rng)),
-                  CellLine = Rle(design$CellLine, lengths(cvg_rng)),
-                  Kit = Rle(as.factor(design$Kit), lengths(cvg_rng)))
-  cvg_rng <- unlist(cvg_rng, use.names = FALSE)
-  mcols(cvg_rng) <- cbind(mcols(cvg_rng), md)
-  cvg_rng <- cvg_rng %>% 
-    mutate(log_score = log2(score + 0.5),
-           strand = strand(target),
-           var = paste0("Kit: ", Kit, " Cellline: ", CellLine))
-  
-  
-  p <- view_coverage(cvg_rng, 
-                     score = log_score, 
-                     colour = feature_type,  
-                     facets = dplyr::vars(var)) +
-    ylim(0, NA) +
-    guides(colour = FALSE) +
-    scale_color_brewer(palette = "Dark2") +
-    labs(subtitle = paste("Coverage over", target$gene_name),
-         y = "Log coverage"
-    ) +
-    theme_bw() +
-    theme(axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          strip.text = element_text(hjust = 0, size = 8),
-          strip.background = element_blank()) +
-    expand_limits(y = 0)
-  
-  segments <- superintronic:::flatten_parts(target)
-  track <- view_segments(segments, color = feature_type) +
-    theme(axis.text.x = element_text(size = 8))
-  
-  patchwork::wrap_plots(p, track, 
-                        ncol = 1, 
-                        heights = c(2, 0.25))
-  
-}
-
 library(plyranges)
 library(superintronic)
 library(ggplot2)
+source(here::here("R", "prettycov.R"))
+
 
 # previously obtained coverage and design
 design <- readRDS(here::here("data", "design.rds"))
 parts <- readRDS(here::here("data", "complete-annotation.rds"))
 cvg <- readRDS(here::here("data", "complete-coverage.rds"))
+names(cvg) <- design$Sample
 
-design <- subset(design, Kit == "polyA")
+design <- subset(design, Kit == "polyA") 
 cvg <- cvg[design$Sample]
 
 
 targets <- filter(parts, 
-                  gene_name %in% c("NBEAL2", "HNRNPL", "HLA-B")
+                  gene_name %in% c("NBEAL2", "HNRNPL", "RPS27A")
 ) %>% 
-  select(gene_id) %>% 
+  select(gene_id, gene_name) %>% 
   as.data.frame()
 
+targets <- targets[c(3,1,2),]
+
+
 fig6_plots <- lapply(seq_len(nrow(targets)),
-                     function(.x) coverage_plot(targets[.x,]))
+                     function(.x) pretty_cov_plot(cvg, parts, targets[.x,], 
+                                                  design, 
+                                                  heights = c(2, 0.25)))
 
 fig6 <- patchwork::wrap_plots(fig6_plots, ncol = 1, guides = "keep")
 
-ggsave(here::here("img/Fig6.pdf"), fig6, height = 7, width = 12)
+ggsave(here::here("img/Fig6.pdf"), fig6, height = 6, width = 10)
